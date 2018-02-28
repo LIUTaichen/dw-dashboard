f467b8cace2694e7e0b5912e74f233632ffa135a
LIUTaichen <liu_taichen@hotmail.com>
2018-02-28 17:02:47 +1300

    base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,35 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import {MatSidenavModule} from '@angular/material/sidenav';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
+import { TopMenuComponent } from './top-menu/top-menu.component';
+import { AppCardComponent } from './app-card/app-card.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
+    TopMenuComponent,
+    AppCardComponent,
   ],
   imports: [
     BrowserModule,
     FlexLayoutModule,
+    MatButtonModule,
+    MatIconModule,
+    MatProgressBarModule,
     MatSidenavModule,
+    MatToolbarModule,
+    MatGridListModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
